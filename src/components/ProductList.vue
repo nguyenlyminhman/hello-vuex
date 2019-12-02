9078bc28b5b83b0a671c90f9c71be8150bda86ae
Man Nguyen Ly Minh <nguyenlyminhman@gmail.com>
2019-12-02 10:51:30 +0700

    add action

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,39 +1,39 @@
 <template>
-    <div>
-        <h1>Product List</h1>
-        <ul>
-            <li v-for="product in products" :key="product.id">
-                {{product.title}} - {{product.price}}
-            </li>
-        </ul>
-    </div>
+  <div>
+    <h1>Product List</h1>
+    <img v-if="loading" src="https://i.imgur.com/JfPpwOA.gif" alt="">
+    <ul v-else>
+      <li v-for="product in products" :key="product.id">{{product.title}} - {{product.price}}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import shop from '@/api/shop'
-import store from '@/store/index'
+// import shop from '@/api/shop'
+import store from "@/store/index";
 
 export default {
-    // data () {
-    //     return {
-    //         products: []
-    //     }
-    // },
+  data() {
+    return {
+      loading: false
+    };
+  },
 
-    computed: {
-        products(){
-            return store.getters.availableProduct
-        }
-    },
-
-    created(){
-        shop.getProducts( products=> {
-            store.commit('setProducts', products)
-        })
+  computed: {
+    products() {
+      return store.getters.availableProduct;
     }
-}
+  },
+
+  created() {
+    // shop.getProducts( products=> {
+    //     store.commit('setProducts', products)
+    // })
+    this.loading = true;
+    store.dispatch("fetchProducts").then(() => (this.loading = false));
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,9 @@
 import Vuex from 'vuex';
 import Vue from 'vue';
+import shop from '@/api/shop';
+
+
+
 
 Vue.use(Vuex);
 
@@ -8,19 +12,39 @@ export default new Vuex.Store({
         products: []
     },
     getters: { // =computed properties
-        availableProduct(state){
+        availableProduct(state) {
             return state.products.filter(product => product.inventory > 0)
         }
     },
-    actions: {
-        fetchProducts(){
+    actions: { // =methods
+        fetchProducts({ commit }) {
             // make the call
             // cal setProducts mutation
+            // shop.getProducts( products=> {
+            //     context.commit('setProducts', products)
+            // })
+            return new Promise((resolve) => {
+                shop.getProducts(products => {
+                    commit('setProducts', products)
+                    resolve();
+                })
+            })
+        },
+
+        addToCart(context, product) {
+            if (product.inventory > 0) {
+                context.commit('pushProductToCart', product)
+            }
+            // else{
+           // // show out of stock message
+
+            // }
         }
     },
 
     mutations: {
-        setProducts(state, products){
+        setProducts(state, products) {
+            // update products
             state.products = products
         }
     }
